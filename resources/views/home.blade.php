5dd96fc242794cef9870b7b8fc1538546cbdefb3
ZairiAimenDz <zairiaimendev@gmail.com>
2021-12-27 16:25:08 +0100

    Minor Changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://raw.githubusercontent.com/4Annee/CoolFrameworkCSS/main/Styles/style.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     <title>Home - ESIBudgeting</title>
 </head>
 <body>
-
+    <div class="hero">
+        <div class="row">
+            <div class="col-4">
+                <h1>Every Dinar Counts</h1>
+            </div>
+            <div class="col-8">
+                <img class="w-100" style="width:100%" src="{{ asset('images/homepage.jpg')}}" alt="Image"/>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
