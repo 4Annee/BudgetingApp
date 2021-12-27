eb32a370b4cb60bf1edbb04081709fe73f2696f9
ZairiAimenDz <zairiaimendev@gmail.com>
2021-12-27 15:53:30 +0100

    added stylesheet

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://raw.githubusercontent.com/4Annee/CoolFrameworkCSS/main/Styles/style.css">
+    <title>Home - ESIBudgeting</title>
+</head>
+<body>
+
+</body>
+</html>
