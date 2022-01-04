cbbed69e39d912114ed92d341131b07a2e23ffda
ZairiAimenDz <zairiaimendev@gmail.com>
2022-01-04 14:09:05 +0100

    Added CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://raw.githubusercontent.com/4Annee/CoolFrameworkCSS/main/Styles/style.css">
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('coolcss/style.css') }}">
     <title>Home - ESIBudgeting</title>
 </head>
 <body>
     <div class="hero">
-        <div class="row">
-            <div class="col-4">
+        <div class="row px-5">
+            <div class="col-4 ">
                 <h1>Every Dinar Counts</h1>
             </div>
             <div class="col-8">
-                <img class="w-100" style="width:100%" src="{{ asset('images/homepage.jpg')}}" alt="Image"/>
+                <img style="width:100%" src="{{ asset('images/homepage.jpg')}}" alt="Image"/>
             </div>
         </div>
     </div>
